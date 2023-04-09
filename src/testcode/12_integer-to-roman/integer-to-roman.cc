6353533c09087d557e534f483c599498390e92f6
YUAN HAO <328899509@qq.com>
2023-04-09 10:14:02 +0800

    12. 整数转罗马数字

--- src/testcode/12_integer-to-roman/integer-to-roman.cc
+++ src/testcode/12_integer-to-roman/integer-to-roman.cc
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <string.h>
 
 int main(int argc, char* argv[]){
+    int int_data = std::atoi(argv[1]);
+    std::vector<int> int_arg = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    std::vector<std::string> str_arg = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    std::string result = "";
 
+    for (int i = 0; i < int_arg.size(); i++)
+    {
+        while(int_data >= int_arg[i]){
+            result = result + str_arg[i];
+            int_data = int_data - int_arg[i];
+        }
+    }
+    std::cout << "The result is :" << result << std::endl;
     return 0;
 }
