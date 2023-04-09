f038fa3c375d49e8e4f0245ff67b69bbd10b62e4
YUAN HAO <328899509@qq.com>
2023-04-09 11:14:41 +0800

    14. 最长公共前缀

--- src/testcode/14_longest-common-prefix/longest-common-prefix.cc
+++ src/testcode/14_longest-common-prefix/longest-common-prefix.cc
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string.h>
+#include <vector>
 
 int main(int argc, char* argv[]){
+    std::vector<std::string> strs = {"flower","flow","flight"};
+    std::string result = "";
+    for (int i =0; i<strs[0].size();i++)
+    {
+        int count = 0;
+        for (int j = 0; j < strs.size(); j++)
+        {
+            if (strs[0][i] == strs[j][i])
+            {
+                count++;
+            }
+            
+        }
 
+        if (count == strs.size())
+        {
+            result = result+ strs[0][i];
+        }else{
+            break;
+        }
+    }
+    std::cout << "The result is: " << result << std::endl;
+    
     return 0;
 }
