bacffdb23acd2c03abacdcd16ef145646cd3c6ff
YUAN HAO <328899509@qq.com>
2023-04-09 10:43:55 +0800

    13. 罗马数字转整数

--- src/testcode/13_roman-to-integer/roman-to-integer.cc
+++ src/testcode/13_roman-to-integer/roman-to-integer.cc
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string.h>
+#include <vector>
+#include <map>
 
 int main(int argc, char* argv[]){
+    std::string roman_data = "MCMXCIV";
+    std::map<char,int> m_arg ={{'M',1000},{'D',500},{'C',100},{'L',50},{'X',10},{'V',5},{'I',1}}; 
+    int result =0;
+
+    for (int i = 0; i < roman_data.size(); i++)
+    {   
+        if(m_arg[roman_data[i]] < m_arg[roman_data[i+1]]){
+            result = result - m_arg[roman_data[i]];
+        }else{
+            result = result + m_arg[roman_data[i]];
+        }
+    }
+    std::cout << "The result value is: " << result << std::endl;
 
     return 0;
 }
