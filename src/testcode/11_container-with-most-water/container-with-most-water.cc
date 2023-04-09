20f10be20bc822ce65d0a8c420b8c4f4c02f1d7e
YUAN HAO <328899509@qq.com>
2023-04-09 09:32:14 +0800

    11. 盛最多水的容器

--- src/testcode/11_container-with-most-water/container-with-most-water.cc
+++ src/testcode/11_container-with-most-water/container-with-most-water.cc
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
 
 int main(int argc, char* argv[]){
+    // std::vector<int> length = {1,8,6,2,5,4,8,3,7};
+    std::vector<int> length = {1};
+    int left = 0;
+    int right = length.size()-1;
+    int temp_result = 0;
+    int result = 0;
 
+    while (left < right)
+    {
+        temp_result = (right - left)*std::min(length[left],length[right]);
+        result = std::max(result, temp_result);
+        std::cout << "result : " << result << " ,temp_result: " << temp_result 
+                  << " ,left: " << left << " ,right: " << right<< std::endl;
+        if(length[left] < length[right]){
+            left++;
+        }else{
+            right--;
+        }
+    }
+    std::cout << "The most water is : " << result << std::endl;
     return 0;
 }
